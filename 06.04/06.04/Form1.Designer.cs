eac81739310d0bf33da756ed4bff24ca44afb967
MarinaKuvshinova <pn-05@ukr.net>
2018-04-11 13:37:29 +0300

    new file

---
+++ 06.04/06.04/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace _06._04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabBox = new System.Windows.Forms.TabControl();
+            this.tabBooks = new System.Windows.Forms.TabPage();
+            this.dataGridBooks = new System.Windows.Forms.DataGridView();
+            this.tabAuthors = new System.Windows.Forms.TabPage();
+            this.dataGridAuthors = new System.Windows.Forms.DataGridView();
+            this.tabPublishers = new System.Windows.Forms.TabPage();
+            this.dataGridPublishers = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.tabBox.SuspendLayout();
+            this.tabBooks.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridBooks)).BeginInit();
+            this.tabAuthors.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridAuthors)).BeginInit();
+            this.tabPublishers.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridPublishers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabBox
+            // 
+            this.tabBox.Controls.Add(this.tabBooks);
+            this.tabBox.Controls.Add(this.tabAuthors);
+            this.tabBox.Controls.Add(this.tabPublishers);
+            this.tabBox.Location = new System.Drawing.Point(12, 12);
+            this.tabBox.Name = "tabBox";
+            this.tabBox.SelectedIndex = 0;
+            this.tabBox.Size = new System.Drawing.Size(366, 239);
+            this.tabBox.TabIndex = 0;
+            // 
+            // tabBooks
+            // 
+            this.tabBooks.Controls.Add(this.dataGridBooks);
+            this.tabBooks.Location = new System.Drawing.Point(4, 22);
+            this.tabBooks.Name = "tabBooks";
+            this.tabBooks.Padding = new System.Windows.Forms.Padding(3);
+            this.tabBooks.Size = new System.Drawing.Size(358, 213);
+            this.tabBooks.TabIndex = 0;
+            this.tabBooks.Text = "Books";
+            this.tabBooks.UseVisualStyleBackColor = true;
+            // 
+            // dataGridBooks
+            // 
+            this.dataGridBooks.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridBooks.Location = new System.Drawing.Point(0, 0);
+            this.dataGridBooks.Name = "dataGridBooks";
+            this.dataGridBooks.Size = new System.Drawing.Size(358, 213);
+            this.dataGridBooks.TabIndex = 0;
+            // 
+            // tabAuthors
+            // 
+            this.tabAuthors.Controls.Add(this.dataGridAuthors);
+            this.tabAuthors.Location = new System.Drawing.Point(4, 22);
+            this.tabAuthors.Name = "tabAuthors";
+            this.tabAuthors.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAuthors.Size = new System.Drawing.Size(358, 213);
+            this.tabAuthors.TabIndex = 1;
+            this.tabAuthors.Text = "Authors";
+            this.tabAuthors.UseVisualStyleBackColor = true;
+            // 
+            // dataGridAuthors
+            // 
+            this.dataGridAuthors.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridAuthors.Location = new System.Drawing.Point(0, 0);
+            this.dataGridAuthors.Name = "dataGridAuthors";
+            this.dataGridAuthors.Size = new System.Drawing.Size(358, 213);
+            this.dataGridAuthors.TabIndex = 0;
+            // 
+            // tabPublishers
+            // 
+            this.tabPublishers.Controls.Add(this.dataGridPublishers);
+            this.tabPublishers.Location = new System.Drawing.Point(4, 22);
+            this.tabPublishers.Name = "tabPublishers";
+            this.tabPublishers.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPublishers.Size = new System.Drawing.Size(358, 213);
+            this.tabPublishers.TabIndex = 2;
+            this.tabPublishers.Text = "Publishers";
+            this.tabPublishers.UseVisualStyleBackColor = true;
+            // 
+            // dataGridPublishers
+            // 
+            this.dataGridPublishers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridPublishers.Location = new System.Drawing.Point(0, 0);
+            this.dataGridPublishers.Name = "dataGridPublishers";
+            this.dataGridPublishers.Size = new System.Drawing.Size(358, 213);
+            this.dataGridPublishers.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(408, 34);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(408, 72);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 2;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(408, 110);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 304);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tabBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabBox.ResumeLayout(false);
+            this.tabBooks.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridBooks)).EndInit();
+            this.tabAuthors.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridAuthors)).EndInit();
+            this.tabPublishers.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridPublishers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabBox;
+        private System.Windows.Forms.TabPage tabBooks;
+        private System.Windows.Forms.DataGridView dataGridBooks;
+        private System.Windows.Forms.TabPage tabAuthors;
+        private System.Windows.Forms.TabPage tabPublishers;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridAuthors;
+        private System.Windows.Forms.DataGridView dataGridPublishers;
+    }
+}
+
---
+++ 06.04/06.04/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace _06._04
+{
+    public partial class Form1 : Form
+    {
+        DataTable dataTableBook = null;
+        DataTable dataTableAuthor = null;
+        DataTable dataTablePublisher = null;
+        public Form1()
+        {
+            InitializeComponent();
+            GetAll(dataGridBooks);
+            GetAll(dataGridAuthors);
+            GetAll(dataGridPublishers);
+        }
+
+        private void GetAll(DataGridView dataGrid)
+        {
+            using (Library2Entities db = new Library2Entities())
+            {
+                if (dataGrid.Name.ToString() == "dataGridBooks")
+                {
+                    dataTableBook = new DataTable();
+                    dataTableBook.Columns.Add("Id");
+                    dataTableBook.Columns.Add("Title");
+                    dataTableBook.Columns.Add("IdAuthor");
+                    dataTableBook.Columns.Add("Pages");
+                    dataTableBook.Columns.Add("Price");
+                    dataTableBook.Columns.Add("IdPublisher");
+                    foreach (var item in db.Book)
+                    {
+                        DataRow dataRow = dataTableBook.NewRow();
+                        dataRow["Id"] = item.Id;
+                        dataRow["Title"] = item.Title;
+                        dataRow["IdAuthor"] = item.IdAuthor;
+                        dataRow["Pages"] = item.Pages;
+                        dataRow["Price"] = item.Price;
+                        dataRow["IdPublisher"] = item.IdPublisher;
+                        dataTableBook.Rows.Add(dataRow);
+                    }
+                    dataGridBooks.DataSource = null;
+                    dataGridBooks.DataSource = dataTableBook;
+                }
+                else if (dataGrid.Name.ToString() == "dataGridAuthors")
+                {
+                    dataTableAuthor = new DataTable();
+                    dataTableAuthor.Columns.Add("Id");
+                    dataTableAuthor.Columns.Add("FirstName");
+                    dataTableAuthor.Columns.Add("LastName");
+                    foreach (var item in db.Author)
+                    {
+                        DataRow dataRow = dataTableAuthor.NewRow();
+                        dataRow["Id"] = item.Id;
+                        dataRow["FirstName"] = item.FirstName;
+                        dataRow["LastName"] = item.LastName;
+                        dataTableAuthor.Rows.Add(dataRow);
+                    }
+                    dataGridAuthors.DataSource = null;
+                    dataGridAuthors.DataSource = dataTableAuthor;
+                }
+                else if (dataGrid.Name.ToString() == "dataGridPublishers")
+                {
+                    dataTablePublisher = new DataTable();
+                    dataTablePublisher.Columns.Add("Id");
+                    dataTablePublisher.Columns.Add("PublisherName");
+                    dataTablePublisher.Columns.Add("Address");
+                    foreach (var item in db.Publisher)
+                    {
+                        DataRow dataRow = dataTablePublisher.NewRow();
+                        dataRow["Id"] = item.Id;
+                        dataRow["PublisherName"] = item.PublisherName;
+                        dataRow["Address"] = item.Address;
+                        dataTablePublisher.Rows.Add(dataRow);
+                    }
+                    dataGridPublishers.DataSource = null;
+                    dataGridPublishers.DataSource = dataTablePublisher;
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (Library2Entities db = new Library2Entities())
+            {
+                if (tabBox.SelectedTab.Name== "tabBooks")
+                {
+                    int index = dataGridBooks.CurrentRow.Index;
+                    Book book = new Book();
+                    book.Title = dataGridBooks["Title", index].Value.ToString();
+                    book.IdAuthor = Convert.ToInt32(dataGridBooks["IdAuthor", index].Value);
+                    book.Pages = Convert.ToInt32(dataGridBooks["Pages", index].Value);
+                    book.Price = Convert.ToInt32(dataGridBooks["Price", index].Value);
+                    book.IdPublisher = Convert.ToInt32(dataGridBooks["IdPublisher", index].Value);
+                    db.Book.Add(book);
+                    db.SaveChanges();
+                    GetAll(dataGridBooks);
+                }
+                else if (tabBox.SelectedTab.Name == "tabAuthors")
+                {
+                    int index = dataGridAuthors.CurrentRow.Index;
+                    Author author = new Author();
+                    author.FirstName = dataGridAuthors["FirstName", index].Value.ToString();
+                    author.LastName = dataGridAuthors["LastName", index].Value.ToString();
+                    db.Author.Add(author);
+                    db.SaveChanges();
+                    GetAll(dataGridAuthors);
+                }
+                else if (tabBox.SelectedTab.Name == "tabPublishers")
+                {
+                    int index = dataGridPublishers.CurrentRow.Index;
+                    Publisher publisher = new Publisher();
+                    publisher.PublisherName = dataGridPublishers["PublisherName", index].Value.ToString();
+                    publisher.Address = dataGridPublishers["Address", index].Value.ToString();
+                    db.Publisher.Add(publisher);
+                    db.SaveChanges();
+                    GetAll(dataGridPublishers);
+                }
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            using (Library2Entities db = new Library2Entities())
+            {
+                if (tabBox.SelectedTab.Name == "tabBooks")
+                {
+                    int index = dataGridBooks.CurrentRow.Index;
+                    int id = Convert.ToInt32(dataGridBooks[0, index].Value);
+                    db.Book.Find(id).Title = dataGridBooks["Title", index].Value.ToString();
+                    db.Book.Find(id).IdAuthor = Convert.ToInt32(dataGridBooks["IdAuthor", index].Value);
+                    db.Book.Find(id).Pages = Convert.ToInt32(dataGridBooks["Pages", index].Value);
+                    db.Book.Find(id).Price = Convert.ToInt32(dataGridBooks["Price", index].Value);
+                    db.Book.Find(id).IdPublisher = Convert.ToInt32(dataGridBooks["IdPublisher", index].Value);
+                    db.SaveChanges();
+                    GetAll(dataGridBooks);
+                }
+                else if (tabBox.SelectedTab.Name == "tabAuthors")
+                {
+                    int index = dataGridAuthors.CurrentRow.Index;
+                    int id = Convert.ToInt32(dataGridAuthors[0, index].Value);
+                    db.Author.Find(id).FirstName = dataGridAuthors["FirstName", index].Value.ToString();
+                    db.Author.Find(id).LastName = dataGridAuthors["LastName", index].Value.ToString();
+                    db.SaveChanges();
+                    GetAll(dataGridAuthors);
+                }
+                else if (tabBox.SelectedTab.Name == "tabPublishers")
+                {
+                    int index = dataGridPublishers.CurrentRow.Index;
+                    int id = Convert.ToInt32(dataGridPublishers[0, index].Value);
+                    db.Publisher.Find(id).PublisherName = dataGridPublishers["PublisherName", index].Value.ToString();
+                    db.Publisher.Find(id).Address = dataGridPublishers["Address", index].Value.ToString();
+                    db.SaveChanges();
+                    GetAll(dataGridPublishers);
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (Library2Entities db = new Library2Entities())
+            {
+                if (tabBox.SelectedTab.Name == "tabBooks")
+                {
+                    if (dataGridBooks.SelectedRows.Count > 0)
+                    {
+                        int index = dataGridBooks.SelectedRows[0].Index;
+                        int id =  Convert.ToInt32(dataGridBooks[0, index].Value);
+                        Book book = db.Book.Find(id);
+                        db.Book.Remove(book);
+                        db.SaveChanges();
+                        GetAll(dataGridBooks);
+                    }
+                }
+                else if (tabBox.SelectedTab.Name == "tabAuthors")
+                {
+                    if (dataGridAuthors.SelectedRows.Count > 0)
+                    {
+                        int index = dataGridAuthors.SelectedRows[0].Index;
+                        int id = Convert.ToInt32(dataGridAuthors[0, index].Value);
+                        Author author = db.Author.Find(id);
+                        db.Author.Remove(author);
+                        db.SaveChanges();
+                        GetAll(dataGridAuthors);
+                    }
+                }
+                else if (tabBox.SelectedTab.Name == "tabPublishers")
+                {
+                    if (dataGridPublishers.SelectedRows.Count > 0)
+                    {
+                        int index = dataGridPublishers.SelectedRows[0].Index;
+                        int id = Convert.ToInt32(dataGridPublishers[0, index].Value);
+                        Publisher publisher = db.Publisher.Find(id);
+                        db.Publisher.Remove(publisher);
+                        db.SaveChanges();
+                        GetAll(dataGridPublishers);
+                    }
+                }
+            }
+        }
+    }
+}
